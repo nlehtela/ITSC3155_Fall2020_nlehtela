981c51bcc0c20491b0d29b721492c92ba9adbde2
Nicholas Lehtela <nlehtela@uncc.edu>
2020-10-13 14:16:26 -0400

    PythonBasics 3 - added logic to starts_with_number function

--- PythonBasics3/pythonBasics3.py
+++ PythonBasics3/pythonBasics3.py
@@ -12,20 +12,26 @@ import re
 # if it starts with a number and false otherwise.
 # (For our purposes, a number is any character that is 0,1,2,3,4,5,6,7,8, or 9.)
 # Note: Be sure to use RegEx!
-def starts_with_number(s):
-  # YOUR CODE HERE
 
-  return
+
+def starts_with_number(s):
+    # YOUR CODE HERE
+    if re.search("^[0-9]", s):
+        return True
+
+    return False
 
 # # Part B. starts_with_consonant
 # Define a function starts_with_consonant(s) that takes a string and returns true
 # if it starts with a consonant and false otherwise.
 # (For our purposes, a consonant is any letter other than A, E, I, O, U.)
 # Note: Be sure to use RegEx and it works for both upper and lower case and for nonletters!
-def starts_with_consonant(s):
-  # YOUR CODE HERE
 
-  return
+
+def starts_with_consonant(s):
+    # YOUR CODE HERE
+
+    return
 
 
 # Part C. binary_multiple_of_4
@@ -34,6 +40,6 @@ def starts_with_consonant(s):
 # Note: Be sure it returns false if the string is not a valid binary number!
 # Hint: Use regular expressions to match for the pattern of a binary number that is a multiple of 4.
 def binary_multiple_of_4(s):
-  # YOUR CODE HERE
+    # YOUR CODE HERE
 
-  return
+    return
