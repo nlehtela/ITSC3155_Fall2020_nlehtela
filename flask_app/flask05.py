0961cd7c721cf99c7c352b5ecd00d94bf14c2adc
Nicholas Lehtela <nlehtela@uncc.edu>
2020-11-17 14:07:55 -0500

    flask app #5 - edit note function handles post and get

--- flask_app/flask05.py
+++ flask_app/flask05.py
@@ -78,11 +78,22 @@ def new_note():
         return render_template('new.html', user=a_user)
 
 
-@app.route('/notes/edit/<note_id>')
+@app.route('/notes/edit/<note_id>', methods=['GET', 'POST'])
 def update_note(note_id):
-    a_user = db.session.query(User).filter_by(email='nlehtela@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('new.html', note=my_note, user=a_user)
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['noteText']
+        note = db.session.query(Note).filter_by(id=note_id).one()
+        note.title = title
+        note.text = text
+        db.session.add(note)
+        db.session.commit()
+        return redirect(url_for('get_notes'))
+    else:
+        a_user = db.session.query(User).filter_by(
+            email='nlehtela@uncc.edu').one()
+        my_note = db.session.query(Note).filter_by(id=note_id).one()
+        return render_template('new.html', note=my_note, user=a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(
