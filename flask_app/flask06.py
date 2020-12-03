8118159434dfbbc89d1da9dd7b030975f37f56e9
Nick <nlehtela@uncc.edu>
2020-12-03 14:37:15 -0500

    adding and displaying comments functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -13,6 +13,8 @@ from forms import RegisterForm
 from flask import session
 import bcrypt
 from forms import LoginForm
+from models import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
 
 app = Flask(__name__)     # create an app
 
@@ -181,6 +183,25 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(
+                note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(
--- flask_app/models.py
+++ flask_app/models.py
@@ -8,6 +8,8 @@ class Note(db.Model):
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    comments = db.relationship(
+        "Comment", backref="note_comment", cascade="all, delete-orphan", lazy=True)
 
     def __init__(self, title, text, date, user_id):
         self.title = title
@@ -23,9 +25,26 @@ class User(db.Model):
     email = db.Column("email", db.String(100))
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
+    note = db.relationship("Note", backref="note", lazy=True)
+    comments = db.relationship("Comment", backref="user_comment", lazy=True)
 
     def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
+        self.registered_on = datetime.date.today()
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.VARCHAR, nullable=False)
+    note_id = db.Column(db.Integer, db.ForeignKey("note.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, content, note_id, user_id):
+        self.date_posted = datetime.date.today()
+        self.content = content
+        self.note_id = note_id
+        self.user_id = user_id
